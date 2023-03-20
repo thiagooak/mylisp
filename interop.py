3e9eb102ee9a7d379a9acd27df8eb803a765b3f1
russolsen <russ@russolsen.com>
2023-03-20 13:48:16 -0400

    Use the new reader.

---
+++ interop.py
@@ -0,0 +1,28 @@
+import importlib as il
+import sym
+
+def do_import(env, mod_name):
+  """Import a Python module full of native functions
+     into the given env. Each native function is
+     bound to mod-name/f-name."""
+  result = {}
+  m = il.import_module(mod_name, sym)
+  names = dir(m)
+  for n in names:
+    name = sym.Symbol.intern(f'{mod_name}/{n}')
+    result[name] = getattr(m, n)
+
+  env.update(result)
+  return result.keys()
+
+def bang(env, f, *args):
+    """Given a form that looks like (! native-f arg1 arg2...)
+    execute the native function and return the result."""
+    return f(*args)
+
+def dot(env, obj, *names):
+    """Given a form that looks like (! py-object field1 field2...)
+    drill down into the object attributes and return the value."""
+    for n in names:
+      obj = getattr(obj, str(n))
+    return obj
